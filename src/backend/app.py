0c0d5d44ea69253ecc3743f1378af2ae05a270db
Pouya Karimi <pouyakarimi86@gmail.com>
2025-12-12 16:16:22 -0500

    prepare for deployment

--- src/backend/app.py
+++ src/backend/app.py
@@ -28,4 +28,5 @@ def root():
     return jsonify({"message": "UWDialed API is running"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.getenv("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
